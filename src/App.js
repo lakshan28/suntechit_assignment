c2bd3c5623e65a5b949c928e83e0a5127929e0b1
lakshan28 <lakshansampath28@gmail.com>
2019-11-24 10:59:09 +0530

    react-router-dom-frist

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Loging from "./components/screens/Loging";
 import Register from "./components/screens/Register";
 import Dashboard from "./components/screens/Dashboard";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
-      <Register />
-    </React.Fragment>
+    <Router>
+      <Route exact path="/" component={Loging} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Dashboard} />
+    </Router>
   );
 }
 
--- src/components/screens/Register.js
+++ src/components/screens/Register.js
@@ -6,67 +6,56 @@ import TextField from "../TextField/TextField";
 
 function Register() {
   return (
-    <Router>
-      <div className="main-register">
-        <div className="register-content">
-          <div className="welcome-back">
-            <div className="harmonee harmoneeeee  ">
-              <div className="harmonee-icon">
-                <GiHummingbird className="harmonee-btn-icon" />
-                <h2 className="harmonee-text">harmonee</h2>
-              </div>
+    <div className="main-register">
+      <div className="register-content">
+        <div className="welcome-back">
+          <div className="harmonee harmoneeeee  ">
+            <div className="harmonee-icon">
+              <GiHummingbird className="harmonee-btn-icon" />
+              <h2 className="harmonee-text">harmonee</h2>
             </div>
-            <p className="welcome-back-title">Welcome Back</p>
-            <p className="keep-title">
-              To keep connected with us please sign in with your personal
-              details.
+          </div>
+          <p className="welcome-back-title">Welcome Back</p>
+          <p className="keep-title">
+            To keep connected with us please sign in with your personal details.
+          </p>
+          <button className="login-button login-button-register">
+            <Link className="nav-lin register-link" to="/login">
+              Login
+            </Link>
+          </button>
+          <p className="register-link-bottom">www.harmonee.com.au</p>
+        </div>
+        <div className="register-text-area">
+          <p className="register-with-harmonee">Register with Harmonee</p>
+          <div className="text-feild-group2">
+            <TextField placeholder="First Name" name="First Name" type="text" />
+            <TextField placeholder="Surname" name="Surname" type="text" />
+            <TextField
+              placeholder="Company Name"
+              name="Company Name"
+              type="text"
+            />
+            <TextField placeholder="Email" name="email" type="email" />
+            <TextField placeholder="Password" name="password" type="password" />
+          </div>
+
+          <label className="todo-container">
+            <p className="input-title input-title2">
+              I agree to terms and conditions and privacy policy.
             </p>
-            <button className="login-button login-button-register">
-              <Link className="nav-lin register-link" to="/login">
-                Login
-              </Link>
-            </button>
-            <p className="register-link-bottom">www.harmonee.com.au</p>
-          </div>
-          <div className="register-text-area">
-            <p className="register-with-harmonee">Register with Harmonee</p>
-            <div className="text-feild-group2">
-              <TextField
-                placeholder="First Name"
-                name="First Name"
-                type="text"
-              />
-              <TextField placeholder="Surname" name="Surname" type="text" />
-              <TextField
-                placeholder="Company Name"
-                name="Company Name"
-                type="text"
-              />
-              <TextField placeholder="Email" name="email" type="email" />
-              <TextField
-                placeholder="Password"
-                name="password"
-                type="password"
-              />
-            </div>
 
-            <label className="todo-container">
-              <p className="input-title input-title2">
-                I agree to terms and conditions and privacy policy.
-              </p>
-
-              <input type="checkbox" />
-              <span className="checkmark checkmark2"></span>
-            </label>
-            <button className="register-button  register-button2">
-              <Link className="nav-lin register-link" to="/register">
-                Register
-              </Link>
-            </button>
-          </div>
+            <input type="checkbox" />
+            <span className="checkmark checkmark2"></span>
+          </label>
+          <button className="register-button  register-button2">
+            <Link className="nav-lin register-link" to="/">
+              Register
+            </Link>
+          </button>
         </div>
       </div>
-    </Router>
+    </div>
   );
 }
 
